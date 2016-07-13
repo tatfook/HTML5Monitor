df9670aa6bbc52f715f66975e2d66745e57be439
marxwolf <marxwolfs@gmail.com>
2016-07-13 15:15:03 +0800

    file iterator added

---
+++ Documentation/fileIterator.py
@@ -0,0 +1,28 @@
+import os 
+import os.path
+import re
+
+rootdir = r'D:\NPL\src\script'
+savefile = 'luafile.txt'
+
+
+#rootdir = r'D:\NPL\src\script\apps\WebServer\admin'
+#savefile = 'pagefile.txt'
+
+
+def fileIterator(filerootdir, savefile):
+	for parent,dir,file in os.walk(filerootdir):
+		f = open(savefile,'a')
+		for filename in file:
+			if (filename.endswith(".lua")):
+				#filetotal = os.path.join(parent,filename)+"\n"
+				luafile = os.path.join(parent,filename)[11:].replace("\\","/")+"\n"
+				
+				f.write(luafile)
+	f.close()
+
+	
+fileIterator(rootdir, savefile)
+
+abc = raw_input("enter to leave:")
+print(abc)
