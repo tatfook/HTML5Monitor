46b0d90901cdef7366ca6d235622eb24bb108d8d
marxwolf <marxwolfs@gmail.com>
2016-07-21 18:34:56 +0800

    connector and imageReceiver

--- script/imageReceiver.lua
+++ script/imageReceiver.lua
@@ -12,29 +12,23 @@ Use Lib:
 NPL.load("(gl)script/ide/System/Encoding/base64.lua");
 local Encoding = commonlib.gettable("System.Encoding");
 
-local imageReceiver = commonlib.gettable("WebServer.imageReceiver");
---[[
-local function AddPublicFiles()
-	NPL.AddPublicFile("script/apps/WebServer/admin/wp-content/pages/imageReceiver.lua", 233);
-end
 
-local function InitServer(host, port)
-	AddPublicFiles();
-	NPL.StartNetServer(host, port);
-end
-]]
-local function imageReceiver.imageSave(imageData, imageSize, imageName)
+local function imageSave(imageData, imageSize, imageName)
 	imageName = imageName or "autoReceived.jpg";
 	imageObj = ParaIO.open(imageName, "w");
-	imageObj:write(imageData, imageSize);
+	imageObj:write(imageData,imageSize);
 	imageObj:close();
 end
 
-local function activate()
+function activate()
+	local msg = msg
 	--InitServer("0.0.0.0", 8099);
 	if(msg) then
 		imageData = Encoding.unbase64(msg.data);
-		imageReceiver.imageSave(imageData, msg.imageSize ,"autoReceived.jpg");
+		LOG.std(nil, "info", "Msg", "msg.dataLength: %d", string.len(imageData));
+		LOG.std(nil, "info", "Msg", "msg.dataType: %s", type(imageData));
+		LOG.std(nil, "info", "Msg", "msg.imageSize: %d", msg.fileSize);
+		imageSave(imageData, msg.fileSize,"autoReceived.jpg");
 	end
 end
 
